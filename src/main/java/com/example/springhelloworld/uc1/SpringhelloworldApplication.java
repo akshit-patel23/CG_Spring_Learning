82cbdeb1ddd32b5ef4d73668802937ea85b50240
akshit-patel23 <akshitpatel.g@gmail.com>
2025-02-24 10:38:13 +0530

    springhello-worlda_pplication

--- src/main/java/com/example/springhelloworld/uc1/SpringhelloworldApplication.java
+++ src/main/java/com/example/springhelloworld/uc1/SpringhelloworldApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringhelloworldApplication {
 
 	public static void main (String[] args) {
-
 		SpringApplication.run(SpringhelloworldApplication.class, args);
 	}
 
